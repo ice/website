4b876237ad23fd3ccf238211b6d2feda88d95bd2
mruz <mruz@poczta.onet.pl>
2018-11-07 07:34:47 +0000

    Composer, use ice 1.3.0

--- tests/install-extension.sh
+++ tests/install-extension.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone -q --depth=1 https://github.com/ice/framework.git
+git clone -q --depth=1 -b 1.3.0 https://github.com/ice/framework.git
 cd framework/
 . ./install
 phpenv config-add ../../tests/ci/ice.ini
